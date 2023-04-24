5e76eb5d2f5e8c938a0bc9e728fb6be9f2c2a0a7
Mary83-ops <nabwiremaryann7@gmail.com>
2023-04-25 00:36:26 +0300

    Adding files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Delicious
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
